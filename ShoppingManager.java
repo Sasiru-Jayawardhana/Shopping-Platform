432e5c14aeeb626c5f13618c7a3852d11d8e5d1d
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2023-12-06 11:42:32 +0530

    Added menu and printProduct methods to ShoppingManager

--- ShoppingManager.java
+++ ShoppingManager.java
@@ -3,4 +3,6 @@ public interface ShoppingManager {
     public abstract void addProduct();
 
     public abstract void removeProduct();
+    public abstract void printProduct();
+    public abstract void menu();
 }
